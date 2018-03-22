d4451b54ea04d09fd2dec137255d193ddfdf7f0f
vyva29 <vyvadil.martin@seznam.cz>
2018-03-22 15:33:51 +0100

    Prvni commit

---
+++ Kuchyne/src/main/java/com/github/vyvm00/Kuchyne/App.java
@@ -0,0 +1,16 @@
+package com.github.vyvm00.Kuchyne;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+  
+      int x = 10;
+      System.out.println(x);
+    	
+    }
+}
